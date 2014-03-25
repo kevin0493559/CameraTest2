369a7a0ab77395610370d855f54ed1a62fe49c8c
Tianxiang Zhang <tz534@nyu.edu>
2014-03-25 12:21:59 -0400

    dd

--- CameraTest/ZTXViewController.m
+++ CameraTest/ZTXViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -38,7 +39,7 @@
     
 }
 - (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
-    NSLog(@"Action Sheet Button Index: %d",buttonIndex);
+   // NSLog(@"Action Sheet Button Index: %d",buttonIndex);
     if (buttonIndex == 0) {
         //Show Photo Library
         @try {
